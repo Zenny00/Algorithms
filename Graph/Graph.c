1621fd199cc6ad0aa3edec35952ae0df22f89221
Zenny <joshuacomfort@gmail.com>
2023-10-17 21:52:17 -0400

    Define structs

--- Graph/Graph.c
+++ Graph/Graph.c
@@ -5,3 +5,23 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+// Macro defining the maximum size of the graph in the number of nodes
+#define MAX_SIZE 10
+
+// Struct to define the graph data structure
+struct Graph {
+	struct Node *head[MAX_SIZE]; // Define array of Node pointers, this is the adjacency list
+};
+
+// Struct to define the node data type
+struct Node {
+	int dest;
+	struct Node *next;
+};
+
+// Defines the edge of a graph from source to destination node
+struct Edge {
+	int src;
+	int dest;
+};
